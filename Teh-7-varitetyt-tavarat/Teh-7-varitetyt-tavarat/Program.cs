61f9f828e675630664bdd44a0615cb212360b93e
KalleK <s2200829@edu.bc.fi>
2024-05-16 21:50:33 +0300

    viimeinen tehtava, 7 vaäritetyt tavarat

---
+++ Teh-7-varitetyt-tavarat/Teh-7-varitetyt-tavarat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+    //public bool checker;
+    public static void Main()
+    {
+
+        Console.CursorVisible = false;
+        //instansitja tulostus
+        VaritettyTavara<Nuoli> varitettuNuoli = new VaritettyTavara<Nuoli>(new Nuoli(), ConsoleColor.Red);
+        //VaritettyTavara<Nuoli>.NaytaTavara;
+        varitettuNuoli.NaytaTavara();
+        VaritettyTavara<Jousi> varitettuJousi = new VaritettyTavara<Jousi>(new Jousi(), ConsoleColor.Green);
+        varitettuJousi.NaytaTavara();
+        VaritettyTavara<Ruoka> varitettuRuoka = new VaritettyTavara<Ruoka>(new Ruoka(), ConsoleColor.Blue);
+        varitettuRuoka.NaytaTavara();
+
+
+        Console.ReadLine();
+    }
+
+}
+
+
---
+++ Teh-7-varitetyt-tavarat/Teh-7-varitetyt-tavarat/VaritettyTavara.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class VaritettyTavara<T>
+{
+    /// <summary>
+    /// /ominaisuudet
+    /// /
+    /// </summary>
+    public T Tavara { get; }
+
+    public ConsoleColor Vari { get;}
+    public VaritettyTavara(T tavara, ConsoleColor vari)//kostru
+    {
+        Tavara = tavara;
+        Vari = vari;
+    }
+    public void NaytaTavara()//nimvarit
+    {
+        ConsoleColor ogVari = Console.ForegroundColor;
+        //varit varin takaisin muuttamiseemn että ei jää siniseks /\  \/ \/ \/
+        Console.ForegroundColor = Vari;
+        Console.WriteLine(Tavara.ToString());
+        Console.ForegroundColor = ogVari;
+    }
+
+}
